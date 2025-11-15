606ad83eb158a596f37b7ee4d013168f0f170428
sar1yeva <yaxsisariyeva55@gmail.com>
2025-11-15 18:03:21 +0000

    Python

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-str1 = "object-oriented "
-str2 = "programming "
-str3 = "with Python"
+str1, str2, str3 = "object-oriented ", "programming ", "with Python"
 print(str1 + str2 + str3)
 
