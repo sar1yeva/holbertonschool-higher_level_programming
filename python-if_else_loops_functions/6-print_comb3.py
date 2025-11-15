75fc1299e1c511abfce14824222ce0e898ca0399
sar1yeva <yaxsisariyeva55@gmail.com>
2025-11-15 19:26:40 +0000

    Python

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(i + 1, 10):
-        print("{:02}".format(i*10 + j), end=", " if not (i == 8 and j == 9) else "\n")
+        end_char = ", " if not (i == 8 and j == 9) else "\n"
+        print("{:02}".format(i*10 + j), end=end_char)
