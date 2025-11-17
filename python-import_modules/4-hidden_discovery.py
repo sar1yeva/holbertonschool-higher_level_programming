b97a3e4bc147618b66f168181ae745291a326550
Yaxshi Sariyeva <yaxsisariyeva55@gmail.com>
2025-11-17 17:55:08 +0400

    Update 4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-import dis
 import marshal
-if __name__ == "__main__":
-    filename = "/tmp/hidden_4.pyc"
-    with open(filename, "rb") as f:
-        f.read(16) 
-        code = marshal.load(f)
-    names = set()
+def find_names(code):
+    names = set(code.co_names)
     for const in code.co_consts:
         if isinstance(const, type(code)):
-            names.update(const.co_names)
-    for name in sorted(names):
+            names.update(find_names(const))
+    return names
+
+if __name__ == "__main__":
+    filename = "/tmp/hidden_4.pyc"
+
+    with open(filename, "rb") as f:
+        f.read(16)  # header-i keç
+        code = marshal.load(f)
+
+    all_names = find_names(code)
+    
+    for name in sorted(all_names):
         if not name.startswith("__"):
             print(name)
