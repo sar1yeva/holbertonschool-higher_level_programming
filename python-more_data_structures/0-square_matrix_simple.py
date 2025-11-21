152f1814eadad173eede8a0671609cfc7d0ea60b
sar1yeva <yaxsisariyeva55@gmail.com>
2025-11-21 06:28:05 +0000

    Python

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[])
-  return list(map(lambda x: x * x,square_matrix_simple))
+    return list(map(lambda row: list(map(lambda x: x * x, row)), matrix))
