f33102009acef3d30073e0711e3b6ca5609f57f1
sar1yeva <yaxsisariyeva55@gmail.com>
2025-11-21 08:09:17 +0000

    Python

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
- for key in sorted(a_dictionary.keys()):
-     update(a_dictionary, new_dict)
-     print("{}: {}".format(key, a_dictionary[key]))
+    a_dictionary[key] = value
