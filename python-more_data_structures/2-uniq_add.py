068176689c552c0d6573262c264feb218d1d4bf8
sar1yeva <yaxsisariyeva55@gmail.com>
2025-11-21 07:03:13 +0000

    Python

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -2,11 +2,8 @@
 def uniq_add(my_list=[]):
     unique_numbers = set()
     total = 0
-
     for number in my_list:
         if number not in unique_numbers:
             unique_numbers.add(number)
             total += number
-
     return total
-
