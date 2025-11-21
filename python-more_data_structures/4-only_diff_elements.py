933f24ee128adc43dc6f63dfae4a4a811f7dd9c4
sar1yeva <yaxsisariyeva55@gmail.com>
2025-11-21 07:51:40 +0000

    Python

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    diff = set()
-    for number in set_1:
-        if number not in set_2:
-            diff.add(number)
-    return diff
+    return list(set_1.symmetric_difference(set_2))
+
