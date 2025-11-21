685728f4daccc0dba1356a3ba44e471ff0242ad5
sar1yeva <yaxsisariyeva55@gmail.com>
2025-11-21 07:50:12 +0000

    Python

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-     diff = set()
+    diff = set()
     for number in set_1:
-        if number in not set_2:
+        if number not in set_2:
             diff.add(number)
     return diff
