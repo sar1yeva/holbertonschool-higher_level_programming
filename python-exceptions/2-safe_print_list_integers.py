46ba76e44624dbd1987e0ffea7c7ab6ba902092b
sar1yeva <yaxsisariyeva55@gmail.com>
2025-11-21 17:36:52 +0000

    Python

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -9,7 +9,6 @@ def safe_print_list(my_list=[], x=0):
                 count += 1
             except (ValueError, TypeError):
                 pass
-     except IndexError:
-         raise
+            i += 1
      print()
      return count
