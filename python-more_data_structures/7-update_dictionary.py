bdeb85e23c6e7bb340433da1c0bbdf23e4a9c1d9
sar1yeva <yaxsisariyeva55@gmail.com>
2025-11-21 08:07:23 +0000

    Python

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+ for key in sorted(a_dictionary.keys()):
+     update(a_dictionary, new_dict)
+     print("{}: {}".format(key, a_dictionary[key]))
