855c8923924dcb98c5142dcb7d96ca23a8ff4e17
sar1yeva <yaxsisariyeva55@gmail.com>
2025-11-28 08:26:04 +0000

    Python

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -7,7 +7,6 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """Square class inheriting from Rectangle."""
-
     def __init__(self, size):
         """
         Initialize a square with a private size attribute.
@@ -15,7 +14,6 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
-
 
     def area(self):
         return self.__size * self.__size
