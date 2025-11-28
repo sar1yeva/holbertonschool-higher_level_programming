76cf7b2ccc9757f036570308fce8dbc04f6fb69c
sar1yeva <yaxsisariyeva55@gmail.com>
2025-11-28 07:58:17 +0000

    Python

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -7,19 +7,10 @@ class Rectangle(BaseGeometry):
     """Rectangle with width and height."""
 
     def __init__(self, width, height):
-        """Initialize rectangle with width and height."""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-    @property
-    def width(self):
-        return self.__width
-
-    @property
-    def height(self):
-        return self.__height
 
     def area(self):
         return self.__width * self.__height
