07c33a880ae60695138ea484096bc31e113ee16b
sar1yeva <yaxsisariyeva55@gmail.com>
2025-11-28 07:23:20 +0000

    Python

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Function that checks if an object is an instance of a class or its subclass."""
+"""
+Function that checks if an object is an instance of a class or its subclass.
+"""
 
 
 def is_kind_of_class(obj, a_class):
