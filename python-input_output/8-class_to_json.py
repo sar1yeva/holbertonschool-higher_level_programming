71a873d01e88ffbf6e14715dbe2f02d9c17d0ec1
sar1yeva <yaxsisariyeva55@gmail.com>
2025-11-28 09:04:26 +0000

    Python

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Function that returns the dictionary description with simple data structure
+for JSON serialization of an object.
+"""
+
+
+def class_to_json(obj):
+    """Return a dictionary representation of obj suitable for JSON serialization.
+
+    Args:
+        obj (any): Instance of a class.
+
+    Returns:
+        dict: Dictionary containing all simple data attributes of obj.
+    """
+    return obj.__dict__.copy()
+
