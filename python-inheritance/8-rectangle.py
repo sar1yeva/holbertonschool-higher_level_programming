e072857bd614b20e35fb54b05e9c9cc32466fbc1
sar1yeva <yaxsisariyeva55@gmail.com>
2025-11-28 07:56:33 +0000

    Python

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,47 +1,28 @@
 #!/usr/bin/python3
-"""This module defines the BaseGeometry class."""
+"""Rectangle class that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class BaseGeometry:
-    """BaseGeometry class with area and integer validation."""
-
-    def area(self):
-        """Raise an exception because area is not implemented."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate that value is an integer greater than 0."""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+class Rectangle(BaseGeometry):
+    """Rectangle with width and height."""
 
     def __init__(self, width, height):
-        """Instantiation with width and height"""
-        self.width = width
-        self.height = height
-
+        """Initialize rectangle with width and height."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
         return self.__width
 
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
     @property
     def height(self):
         return self.__height
 
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
