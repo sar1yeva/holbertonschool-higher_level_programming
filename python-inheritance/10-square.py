231f963abeb309ab5869daee74d89dcd47a6329d
sar1yeva <yaxsisariyeva55@gmail.com>
2025-11-28 08:09:07 +0000

    Python

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Rectangle class that inherits from BaseGeometry."""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+BaseGeometry = __import__('9-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
