30b7bcbd3d4b91f8910aa8f5264efb6476fedfb1
sar1yeva <yaxsisariyeva55@gmail.com>
2025-11-28 09:05:44 +0000

    Python

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Function that returns the dictionary description with simple data structure
+Function that returns the dictionary
+description with simple data structure
 for JSON serialization of an object.
 """
 
