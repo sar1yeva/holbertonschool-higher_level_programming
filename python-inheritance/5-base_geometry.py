8ce9de6340a734ccb5d79572a3774b299529f3f0
sar1yeva <yaxsisariyeva55@gmail.com>
2025-11-28 07:30:35 +0000

    Python

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-class BaseGeometry():
-      pass
+"""
+Defines an empty BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """Empty class representing base geometry."""
+    pass
+
