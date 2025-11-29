fc6f156be220d63d28f0993065be8167c5259131
sar1yeva <yaxsisariyeva55@gmail.com>
2025-11-29 20:06:01 +0000

    SQL

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,14 @@
 -- comedy_only
-SELECT s.title
-FROM tv_shows s
-JOIN tv_show_genres sg ON s.id = sg.show_id
-JOIN genres g ON sg.genre_id = g.id
-WHERE g.name = 'Comedy'
-ORDER BY s.title ASC;
+SELECT
+    tv_shows.title
+FROM
+    tv_shows
+JOIN tv_show_genres
+    ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres
+    ON tv_genres.id = tv_show_genres.genre_id
+WHERE
+    tv_genres.name = 'Comedy'
+ORDER BY
+    tv_shows.title ASC;
+
