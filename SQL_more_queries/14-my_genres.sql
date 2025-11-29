bfba48a79bf72ce6b5d342f86b6c46493eb4b7f1
sar1yeva <yaxsisariyeva55@gmail.com>
2025-11-29 20:01:51 +0000

    SQL

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,14 @@
 -- my_genres
-SELECT g.name
-FROM genres g
-JOIN tv_show_genres sg ON g.id = sg.genre_id
-JOIN tv_shows s ON sg.show_id = s.id
-WHERE s.title = 'Dexter'
-ORDER BY g.name ASC;
+SELECT
+    tv_genres.name
+FROM
+    tv_genres
+JOIN tv_show_genres
+    ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows
+    ON tv_shows.id = tv_show_genres.show_id
+WHERE
+    tv_shows.title = 'Dexter'
+ORDER BY
+    tv_genres.name ASC;
+
