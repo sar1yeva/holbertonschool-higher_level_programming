946a92f0624bad1e2e62917c405fb5a4fe745c54
sar1yeva <yaxsisariyeva55@gmail.com>
2025-11-29 20:00:15 +0000

    SQL

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,13 @@
 -- count_shows_by_genre
-SELECT g.name AS genre, COUNT(sg.show_id) AS number_of_shows
-FROM genres g
-JOIN tv_show_genres sg ON g.id = sg.genre_id
-GROUP BY g.name
-ORDER BY number_of_shows DESC;
+SELECT
+    tv_genres.name AS genre,
+    COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM
+    tv_genres
+JOIN tv_show_genres
+    ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY
+    tv_genres.id
+ORDER BY
+    number_of_shows DESC;
+
