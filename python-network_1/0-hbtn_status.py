0318c69f57d19a6673b7bfe0a442a36393c8a566
sar1yeva <yaxsisariyeva55@gmail.com>
2025-12-01 11:07:06 +0000

    PythonN

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python3
 import urllib.request
-
+'''fetches https://intranet.hbtn.io/status'''
 url = "https://intranet.hbtn.io/status"
 with urllib.request.urlopen(url) as response:
     data = response.read()
