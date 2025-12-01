eb27b16121a65a342bc2c00af7638b13b140b6d8
sar1yeva <yaxsisariyeva55@gmail.com>
2025-12-01 11:29:18 +0000

    PythonN

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 This script takes in a URL, sends a request to the URL,
-and displays the value of the X-Request-Id variable found in the header of the response.
+and displays the value of the X-Request-Id
+variable found in the header of the response.
 """
 
 import sys
